e1f2445713b739d4c3c01515ff1278bdc84f9390
Dima <Semonovdis>
2022-11-19 20:35:47 +0300

    Work4

---
+++ Work4/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+int N = new int();
+Console.WriteLine("Enter the Number");
+N= Convert.ToInt32(Console.ReadLine());
+int i = 0;
+Console.WriteLine("All even numbers");
+while (i < N - 1)
+{
+    i += 2;
+    Console.WriteLine(i);
+}
